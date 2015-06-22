a2896b864cbe7774cdb05a75cd37e1eb9cdcddfe
kalikadze <jozef.kalianko@gmail.com>
2015-06-22 22:57:48 +0200

    functionality implement
    - implemented both function (absolut, relative time off)

--- AutoOff/Form1.cs
+++ AutoOff/Form1.cs
@@ -7,12 +7,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 
 namespace AutoOff
 {
 
     public partial class Auto_Off_Form : Form
-    {    
+    {
+        bool first_tick = true;
+        DateTime time_abs = DateTime.Now, current;
+        TimeSpan time_rel;
+
         public Auto_Off_Form()
         {
             InitializeComponent();
@@ -31,7 +36,7 @@ namespace AutoOff
                                                  "Or you may keep it (today) and change just a time by clicking\n" +
                                                  "on the hours, minutes, seconds. You may directly insert desired\n" +
                                                  "value or use arrows up/downs.");
-            toolTip2.SetToolTip(dateTimePicker1, "Pick date by clicking on the arrrow. Pick desired date.\n" +
+            toolTip2.SetToolTip(dateTimePicker2, "Pick date by clicking on the arrrow. Pick desired date.\n" +
                                                  "Or you may keep it (today) and change just a time by clicking\n" +
                                                  "on the hours, minutes, seconds. You may directly insert desired\n" +
                                                  "value or use arrows up/downs.");
@@ -40,6 +45,7 @@ namespace AutoOff
 
         private void button_start_Click(object sender, EventArgs e)
         {
+            first_tick = true;
             main_timer.Enabled = true;
             button_stop.Enabled = true;
             button_start.Enabled = false;
@@ -48,6 +54,7 @@ namespace AutoOff
 
         private void button_stop_Click(object sender, EventArgs e)
         {
+            first_tick = false;
             main_timer.Enabled = false;
             button_start.Enabled = true;
             button_stop.Enabled = false;
@@ -56,13 +63,64 @@ namespace AutoOff
 
         private void main_timer_Tick(object sender, EventArgs e)
         {
+            int cmp_result = 0;
+        
+                        
+            // result of absolute
             if (tabControl1.SelectedTab == tabControl1.TabPages["absolute"])
-            { 
-            
+            {
+                if (first_tick)
+                {
+                    time_abs = dateTimePicker1.Value;
+                    current = DateTime.Now;
+                    first_tick = false;
+                }                
             }
+
+            // result of relative
             if (tabControl1.SelectedTab == tabControl1.TabPages["relative"])
             {
+                if (first_tick)
+                {
+                    time_rel = new TimeSpan(dateTimePicker2.Value.Hour, dateTimePicker2.Value.Minute, dateTimePicker2.Value.Second);
+                    time_abs = DateTime.Now.Add(time_rel);
+                    first_tick = false;
+                }                
+            }
 
+            current = DateTime.Now;
+            cmp_result = DateTime.Compare(time_abs, current);
+
+            if (cmp_result <= 0)
+            {
+                // off
+                if (radioButton1_TO.Checked || radioButton8_TO.Checked)
+                {
+                    var psi = new ProcessStartInfo("shutdown","/s /t 0");
+                    psi.CreateNoWindow = true;
+                    psi.UseShellExecute = false;
+                    Process.Start(psi);                
+                }
+
+                // sleep & standby
+                if (radioButton2_S.Checked || radioButton7_S.Checked)
+                {
+                    Application.SetSuspendState(PowerState.Suspend, true, true);  
+                }
+                
+                // hibernate
+                if (radioButton3_H.Checked || radioButton6_H.Checked)
+                {
+                    Application.SetSuspendState(PowerState.Hibernate, true, true);
+                }
+                // restart
+                if (radioButton4_R.Checked || radioButton5_R.Checked)
+                {
+                    var psi = new ProcessStartInfo("shutdown", "/r /t 0");
+                    psi.CreateNoWindow = true;
+                    psi.UseShellExecute = false;
+                    Process.Start(psi);
+                }                
             }
         }
     }
